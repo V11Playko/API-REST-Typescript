536807cfb1db0955a7928cc47b7d271c7b7cec7c
Heinner Vega <heinnervega20@gmail.com>
2024-03-15 16:19:24 -0500

    Configure basic Express server with CORS in app.ts

---
+++ src/app.ts
@@ -0,0 +1,9 @@
+import "dotenv/config"
+import express from "express"
+import cors from "cors"
+
+const PORT = process.env.PORT || 3001
+const app = express()
+app.use(cors())
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
