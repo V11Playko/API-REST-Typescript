df4f1ce87ddc798e79f7fc8a2ecf6989ee68761c
Heinner Vega <heinnervega20@gmail.com>
2024-03-15 21:26:21 -0500

    Error handling in dynamic import of route modules

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,12 @@ import "dotenv/config"
 import express from "express"
 import cors from "cors"
 
+import { router } from "./routes"
+
 const PORT = process.env.PORT || 3001
 const app = express()
+
 app.use(cors())
+app.use('/api', router)
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,19 +1,27 @@
 import { Router } from "express";
-import { readdirSync} from "fs"
+import { readdirSync } from "fs";
 
-const PATH_ROUTER = `${__dirname}`
-const router = Router()
+const PATH_ROUTER = `${__dirname}`;
+const router = Router();
 
 const cleanFileName = (fileName: string) => {
     const file = fileName.split(".").shift();
-    return file
-}
+    return file;
+};
 
-readdirSync(PATH_ROUTER).filter((fileName) => {
-    const cleanName = cleanFileName(fileName)
+readdirSync(PATH_ROUTER).forEach((fileName) => {
+    const cleanName = cleanFileName(fileName);
     if (cleanName !== "index") {
-        router.use(`/${cleanName}`)
+        import(`./${cleanName}`).then((moduleRouter) => {
+            router.use(`/${cleanName}`, moduleRouter.router);
+        }).catch((error) => {
+            console.error(`Error importing module ${cleanName}:`, error);
+            // Deshabilitar la ruta en caso de error
+            router.use(`/${cleanName}`, (_req, res, next) => {
+                res.status(500).send("Internal Server Error");
+            });
+        });
     }
-})
+});
 
-export { router }
+export { router };
