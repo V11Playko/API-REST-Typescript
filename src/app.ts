a513ca8dcd704e4ba3d29fcee71b467be24a65fa
Heinner Vega <heinnervega20@gmail.com>
2024-03-16 23:20:00 -0500

    Add conection mongo

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import express from "express"
 import cors from "cors"
 
 import { router } from "./routes"
+import db from "./configuration/mongo"
 
 const PORT = process.env.PORT || 3001
 const app = express()
@@ -10,4 +11,5 @@ const app = express()
 app.use(cors())
 app.use('/api', router)
 
+db().then(() => console.log('Conexion Ready'))
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
