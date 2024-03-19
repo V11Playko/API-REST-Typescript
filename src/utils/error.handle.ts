a214d2fc60e96407f1a72682d2ea001fce9b3d55
Heinner Vega <heinnervega20@gmail.com>
2024-03-18 19:22:12 -0500

    Updated utils/error.handle

--- src/utils/error.handle.ts
+++ src/utils/error.handle.ts
@@ -1,7 +1,8 @@
 import { Response } from "express"
 
 
-const handleHttp = (res: Response, error: string) => {
+const handleHttp = (res: Response, error: string, errorRaw?: any) => {
+    console.log(errorRaw)
     res.status(500)
     res.send({ error })
 }
