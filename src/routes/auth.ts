6490da3fb3c1c80727574d116a3919519f9b5a68
Heinner Vega <heinnervega20@gmail.com>
2024-03-20 10:37:30 -0500

    Fix error previous commitment

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -6,5 +6,4 @@ const router = Router();
 router.post("/register", registerCtrl);
 
 
-
 export { router }
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,10 +1,11 @@
+import { json } from "express";
 import { User } from "../interfaces/user.interface";
 import UserModel from "../models/user.models";
 import { encrypt } from "../utils/bcrypt.handle";
 
 const registerNewUser = async ({email, password, name}: User) => {
     const checkIs = await UserModel.findOne({email})
-    if (checkIs) return "ALREADY_USER";
+    if (checkIs) return "A person with that mail already exists";
 
     const passHash = await encrypt(password)
     const registerNewUser = await UserModel.create({
